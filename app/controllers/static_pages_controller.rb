97fc355f163b279f883ce926abb492b05b1ee666
Kylemainstreetcodes <kkelley56@gmail.com>
2018-10-03 18:53:22 -0500

    First Commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,11 @@
+class StaticPagesController < ApplicationController
+  def about
+    render "about"
+  end
+  def contact
+    render "contact"
+  end
+  def portfolio
+    render "portfolio"
+  end
+end
